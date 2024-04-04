f6798c888e9c7e3df27b81aabc9c7e643729c9d6
Stepan Dilman <stepan.dilman@gmail.com>
2024-04-04 13:28:27 +0500

    Add a portion of tests for API /films/

--- src/tests/functional/src/test_film.py
+++ src/tests/functional/src/test_film.py
@@ -1,6 +1,7 @@
 import uuid
 import pytest
 import time
+import json
 
 from http import HTTPStatus
 
@@ -13,8 +14,12 @@ es_index = 'movies'
 @pytest.fixture
 def test_films_search_data():
     data = [
-        {'id': str(uuid.uuid4()), 'title': 'wow movie', 'description': '', 'imdb_rating': 9.0, 'genres': []},
-        {'id': str(uuid.uuid4()), 'title': 'so-so movie', 'description': '', 'imdb_rating': 6.5, 'genres': []},
+        {'id': str(uuid.uuid4()), 'title': 'wow movie', 'description': '', 
+         'imdb_rating': 9.0, 'genres': ['Sci-Fi', 'Adventure']},
+        {'id': str(uuid.uuid4()), 'title': 'so-so movie', 'description': '', 
+         'imdb_rating': 6.5, 'genres': []},
+        {'id': str(uuid.uuid4()), 'title': 'doc movie', 'description': '',
+          'imdb_rating': 6.5, 'genres': ['Documentary']},
     ]
     return data
 
@@ -41,3 +46,43 @@ async def test_films_search_query_missing(http_session, es_write_data, get_list_
 
     assert status == HTTPStatus.NOT_FOUND
     assert res == { "detail": "films not found" }
+
+@pytest.mark.asyncio
+async def test_films_search_query_sort(http_session, es_write_data, get_list_data_from_api, test_films_search_data):
+    await es_write_data(test_films_search_data, es_index)
+    sort_term = "-imdb_rating"
+    time.sleep(3)
+    url = f'http://{test_settings.FASTAPI_HOST}:{test_settings.FASTAPI_PORT}' \
+          f'/api/v1/films/?sort={sort_term}&page=1&size=50'
+    res, headers, status = await get_list_data_from_api(url)
+    data_sorted = sorted(test_films_search_data, key=lambda x: x['imdb_rating'], reverse=True)
+
+    assert status == HTTPStatus.OK
+    assert len(res) == len(data_sorted)
+    res[0]['imdb_rating'] == data_sorted[0]['imdb_rating']
+    res[1]['imdb_rating'] == data_sorted[1]['imdb_rating']
+
+@pytest.mark.asyncio
+async def test_films_search_query_genre(http_session, es_write_data, get_list_data_from_api, test_films_search_data):
+    await es_write_data(test_films_search_data, es_index)
+    genre = "Documentary"
+    time.sleep(3)
+    url = f'http://{test_settings.FASTAPI_HOST}:{test_settings.FASTAPI_PORT}' \
+          f'/api/v1/films/?genre={genre}&page=1&size=50'
+    res, headers, status = await get_list_data_from_api(url)
+
+    assert status == HTTPStatus.OK
+    assert len(res) == 1
+
+
+@pytest.mark.asyncio
+async def test_films_search_id(http_session, es_write_data, get_data_from_api, test_films_search_data):
+    await es_write_data(test_films_search_data, es_index)
+    film_id = test_films_search_data[0]['id']
+    time.sleep(3)
+    url = f'http://{test_settings.FASTAPI_HOST}:{test_settings.FASTAPI_PORT}' \
+          f'/api/v1/films/{film_id}'
+    body, headers, status = await get_data_from_api(url)
+
+    assert status == HTTPStatus.OK
+    assert body['id'] == film_id
